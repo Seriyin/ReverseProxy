6abf396028ae16a92b59609198188d5670a4e340
André <redan.blue27@gmail.com>
2017-04-20 15:13:35 +0100

    Fleshed out MonitorUDP.
    MonitorUDP now generates an helloer thread inside itself so it can
    constructHelloPackets both as the first welcome and for periodic helloing.
    
    MonitorUDP will need to negotiate the timeout for receiving responses
    since it's configured server side and as such should send an ACK when
    it's effective.

--- MonitorUDP/src/monitorudp/MonitorUDP.java
+++ MonitorUDP/src/monitorudp/MonitorUDP.java
@@ -9,12 +9,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Andre, Matias, Nuno
  */
-public class MonitorUDP 
+public class MonitorUDP implements Runnable
 {
     private final DatagramSocket ServerSocket;
     private final DatagramPacket CurrentPacket;
@@ -28,7 +31,7 @@ public class MonitorUDP
         {
             IP = InetAddress.getByName(address);
         }
-        catch(Exception e) 
+        catch(UnknownHostException e) 
         {
             IP=null;
             System.err.println(e.getMessage());
@@ -42,6 +45,36 @@ public class MonitorUDP
             CurrentPacket=null;
         }
     }
+
+    public MonitorUDP(DatagramSocket ServerSocket, DatagramPacket CurrentPacket) 
+    {
+        this.ServerSocket = ServerSocket;
+        this.CurrentPacket = CurrentPacket;
+    }
+    
+    @Override
+    public void run() 
+    {
+        try 
+        {
+            while(true) 
+            {
+                constructPacketHello();
+                try 
+                {
+                    ServerSocket.send(CurrentPacket);
+                } 
+                catch (IOException ex) 
+                {
+                    Logger.getLogger(MonitorUDP.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e) {}
+    }
+
+    
     
     /**
      * @param args the command line arguments
@@ -53,22 +86,84 @@ public class MonitorUDP
             MonitorUDP Monitor = new MonitorUDP(5555,args[1]);
             Monitor.runMonitor();
         }
-        catch(IOException e) 
+        catch(IOException | InterruptedException e) 
         {
             System.err.println(e.getMessage());
         }
     }
     
-    private void runMonitor() throws IOException
+    private void runMonitor() throws IOException, InterruptedException
     {
-        while(true) 
+        constructPacketHello();
+        ServerSocket.send(CurrentPacket);
+        ServerSocket.setSoTimeout(15000);
+        try
         {
-            constructPacket();
+            ServerSocket.receive(CurrentPacket);
+        }
+        catch(IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        int timeout=getTimeoutPacket();
+        if (timeout!=-1) 
+        {
+            ServerSocket.setSoTimeout(timeout);
+            boolean bPacketReceiveFail=false;
+            Thread hello=new Thread(new MonitorUDP(ServerSocket,CurrentPacket));
+            int count=0;
+            while(count<3) 
+            {
+                try
+                {
+                    ServerSocket.receive(CurrentPacket);
+                }
+                catch(IOException e)
+                {
+                    bPacketReceiveFail = true;
+                }
+                if (!bPacketReceiveFail) 
+                {
+                    constructPacketResponse();
+                }
+                else 
+                {
+                    count++;
+                }
+            }
             ServerSocket.send(CurrentPacket);
+            hello.interrupt();
+            hello.join(15000);
+        }
+        else 
+        {
+            throw new RuntimeException();
         }
     }
 
-    private void constructPacket() 
+    
+    /**
+     * Have to set the byte array to a single hello byte.
+     */
+    private void constructPacketHello() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Read from the current packet the timeout if its there, otherwise
+     * @return 
+     */
+    private int getTimeoutPacket() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Build a packet byte array with the new integer for the count and appropriate
+     * flag.
+     */
+    private void constructPacketResponse() 
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
